70235bdd2a98efc9bed889c5882954d2ab0ce8ee
Tony <114925619+TonyCleriston@users.noreply.github.com>
2022-12-03 00:24:36 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,8 @@
+function cadastro() {
+    document.getElementsByClassName("telahorario")[0].style.display = "none" 
+    document.getElementsByClassName("telacadastro")[0].style.display = "flex"   
+ }
+ function horario() {
+     document.getElementsByClassName("telahorario")[0].style.display = "flex" 
+     document.getElementsByClassName("telacadastro")[0].style.display = "none"   
+  }
